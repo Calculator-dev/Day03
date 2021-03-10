3e771c2684ff10a1bacaee03fe3b197a06b0de62
haris <haris_aksalic@hotmail.com>
2021-03-10 18:56:03 +0100

    Adding exercise ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+  // Only change code below this line
+  // This is one way to comment in Javascript
+  /* This is the second way to comment in Javascript*/
+  // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
